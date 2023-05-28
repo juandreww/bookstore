8d800c23bd50622d6f97fb99b0f17435458c3830
Juan Andrew <juandreww@outlook.com>
2023-05-29 06:57:42 +0700

    application

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Bookstore
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
+    # Configuration for the applcation, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
