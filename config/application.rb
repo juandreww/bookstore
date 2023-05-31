5ef1b8798930fdc5c8829de4d01165eb4990638f
Juan Andrew <juandreww@outlook.com>
2023-05-31 07:06:34 +0700

    version

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Bookstore
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    # Initialize configuration defaults for originally generated Rails version
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
