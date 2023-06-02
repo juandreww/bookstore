276a4cebfeed8c2ea4732f41e933704a2cd86d5d
Juan Andrew <juandreww@outlook.com>
2023-06-02 20:58:20 +0700

    thread safe

--- design_pattern/singleton/naive.rb
+++ design_pattern/singleton/naive.rb
@@ -8,12 +8,13 @@ class Singleton
   end
 
   def some_business_logic
-
+    puts 'Any business logic'
   end
 end
 
 s1 = Singleton.instance
 s2 = Singleton.instance
+s2.some_business_logic
 
 if s1.equal?(s2)
   puts 'Singleton works, both variables contain the same instance.'
